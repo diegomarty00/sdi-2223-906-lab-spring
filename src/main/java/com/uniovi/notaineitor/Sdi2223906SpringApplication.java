d14ea67be90717c52f994d4938f902e1800a3051
uo270457 <uo270457@uniovi.es>
2023-02-06 10:21:55 +0100

    SDI-2223-906-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/notaineitor/Sdi2223906SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaineitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223906SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223906SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/notaineitor/Sdi2223906SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaineitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223906SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
